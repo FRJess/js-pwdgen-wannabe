b17bea95f43dece087bb469d7581b8d2e07b6503
FR_Jess <fr_jess@icloud.com>
2022-10-11 09:33:44 +0200

    [replace] const with let

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Chiedere all’utente il suo nome
 
-const nomeUtente = prompt("Inserisci il tuo nome");
+let nomeUtente = prompt("Inserisci il tuo nome");
 
 // Chiedere all’utente il suo cognome
 
-const cognomeUtente = prompt("Inserisci il tuo cognome");
+let cognomeUtente = prompt("Inserisci il tuo cognome");
 
 // Chiedere all’utente il suo colore preferito
 
-const colorePreferito = prompt("Inserisci il tuo colore preferito");
+let colorePreferito = prompt("Inserisci il tuo colore preferito");
 
 // Generare la password
 
